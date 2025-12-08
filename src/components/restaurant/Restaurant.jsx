f929b35d936ba4845a087bb634a64eac48bdfa70
dmitrypolyakov <dmitry.polyakovpro@gmail.com>
2025-12-08 23:42:38 +0300

    second homework

--- src/components/restaurant/Restaurant.jsx
+++ src/components/restaurant/Restaurant.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {List} from "../list/List.jsx";
+import {ReviewForm} from "../review-form/ReviewForm.jsx";
 
 export function Restaurant({name, menuName, listOfDishes, reviewName, listOfReviews, id}) {
     if (!name) {
@@ -10,6 +11,7 @@ export function Restaurant({name, menuName, listOfDishes, reviewName, listOfRevi
             <h2>{name}</h2>
             <List listName={menuName} list={listOfDishes} isCount={true}/>
             <List listName={reviewName} list={listOfReviews} isCount={false}/>
+            <ReviewForm />
         </div>
     );
 };
