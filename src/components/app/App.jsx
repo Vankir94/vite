c9fcdc2187cabc3b3d3a3638c902bc6487be64bf
Vankir94 <67509982+Vankir94@users.noreply.github.com>
2025-12-09 23:22:34 +0300

    add Ionic lib

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,3 +1,4 @@
+import { IonApp } from '@ionic/react';
 import {Layout} from "../layout/Layout";
 import {Tabs} from "../tabs/Tabs.jsx";
 import MY_RESTAURANTS from "../../materials/mock.js";
@@ -5,11 +6,13 @@ import React from "react";
 
 export const App = ({title}) => {
     return (
-        <Layout>
-            <div>
-                <h1>{title}</h1>
-                <Tabs restaurants={MY_RESTAURANTS} />
-            </div>
-        </Layout>
+        <IonApp>
+            <Layout>
+                <div>
+                    <h1>{title}</h1>
+                    <Tabs restaurants={MY_RESTAURANTS} />
+                </div>
+            </Layout>
+        </IonApp>
     );
 };
