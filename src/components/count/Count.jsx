19befb36f6789caf98d677c772d76f2c7ec9a46f
Vankir94 <67509982+Vankir94@users.noreply.github.com>
2025-12-09 23:02:54 +0300

    Update Count component

--- src/components/count/Count.jsx
+++ src/components/count/Count.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import {useCount} from "./use-count.js";
 
 export function Count() {
     const {value, increase, decrease} = useCount({minValue: 0, maxValue: 5});
+
+    useEffect(() => {
+        console.log(value);
+        return () => console.log('unmount');
+    }, [value])
     return (
         <div>
             <button onClick={increase}>+</button>
